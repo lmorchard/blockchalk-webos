c9db0916b19882635559fb91fb0d3de9a6cb35a2
Leslie Michael Orchard <l.m.orchard@pobox.com>
2010-05-17 00:18:19 -0400

    Chalkback field at bottom of chalk scene; scrolling to reveal selected chalk in thread list; pop-up menu on thread list tap (reply, report)

--- src/app/assistants/chalk-assistant.js
+++ src/app/assistants/chalk-assistant.js
@@ -11,6 +11,8 @@ function ChalkAssistant(chalk) {
 
 ChalkAssistant.prototype = (function () { /** @lends ChalkAssistant# */
 
+    var MAX_MESSAGE_LENGTH = 256;
+
     return {
 
         /**
@@ -20,6 +22,11 @@ ChalkAssistant.prototype = (function () { /** @lends ChalkAssistant# */
 
             BlockChalk.setupGlobalMenu(this.controller);
 
+            this.model = {
+                message: '',
+                submit_disabled: false
+            };
+
             this.controller.get('contents').update(
                 this.chalk.contents.escapeHTML()
             );
@@ -28,19 +35,6 @@ ChalkAssistant.prototype = (function () { /** @lends ChalkAssistant# */
                 BlockChalk.formatDate(this.chalk.datetime),
                 this.chalk.distance
             ].join(' ').escapeHTML());
-
-            /*
-            if (['nearby','home'].indexOf(BlockChalk.service.getLocationContext()) === -1) {
-                // User browsing another location, disallow chalkbacks.
-                this.controller.get('chalkback-wrapper').remove();
-                this.controller.get('reply-wrapper').width = "100%";
-            } else {
-                // Chalkbacks allowed here, so setup up the button.
-                this.controller.setupWidget(
-                    'chalk-chalkback-button', { label: $L('Chalkback') }, {}
-                );
-             }
-             */
 
             // Hide the location button, or set it up if a place is present.
             /*
@@ -71,27 +65,66 @@ ChalkAssistant.prototype = (function () { /** @lends ChalkAssistant# */
                 this.threadlist_model
             );
 
+            if (['nearby','home'].indexOf(BlockChalk.service.getLocationContext()) !== -1) {
+                this.controller.get('chalk-scene')
+                    .addClassName('can-chalkback');
+                this.controller.setupWidget(
+                    'chalk-message', 
+                    {
+                        modelProperty: 'message',
+                        hintText: $L('Write a comment...'),
+                        multiline: true,
+                        focus: false,
+                        enterSubmits: true,
+                        textFieldName: "chalk-message",
+                        className: " ",
+                        changeOnKeyPress: true
+                    }, 
+                    this.model
+                );
+            }
+
+            Decafbad.Utils.setupLoadingSpinner(this);
+
+            this.refresh();
+        },
+
+        /**
+         * Refresh the chalk thread list in the scene.
+         */
+        refresh: function (bottom) {
+            // Reset the post field.
+            this.model.message = '';
+            this.controller.modelChanged(this.model);
+            this.model.submit_disabled = false;
+            this.updateRemainingChars();
+
+            Decafbad.Utils.showLoadingSpinner(this);
+
             BlockChalk.service.getThreadChalks(
                 BlockChalk.gps_fix, 
                 this.chalk.threadId,
-                this.updateThreadDisplay.bind(this),
+                function (chalks) {
+                    Decafbad.Utils.hideLoadingSpinner(this);
+                    this.updateThreadDisplay(chalks, bottom);
+                }.bind(this),
                 function () {
+                    Decafbad.Utils.hideLoadingSpinner(this);
                     // TODO: Error handler.
-                }
+                }.bind(this)
             );
-
         },
 
         /**
          * Update the thread display from a list of chalks.
          */
-        updateThreadDisplay: function (chalks) {
+        updateThreadDisplay: function (chalks, bottom) {
 
             // Fixup items with a few status flags, process dates, then sort by
             // timestamps in reverse chron order.
             this.threadlist_model.items = chalks
                 .filter(function (chalk) {
-                    return chalk.id != this.chalk.threadId
+                    return chalk.id != this.chalk.threadId;
                 }.bind(this))
                 .map(function (chalk) {
                     chalk.isCurrent = (this.chalk.id == chalk.id) ?
@@ -121,6 +154,20 @@ ChalkAssistant.prototype = (function () { /** @lends ChalkAssistant# */
             var thread_list = this.controller.get('threadlist');
             thread_list.mojo.noticeUpdatedItems(0, this.threadlist_model.items);
             thread_list.mojo.setLength(this.threadlist_model.items.length);
+
+            var scroller = this.controller
+                .get('mojo-scene-chalk-scene-scroller').mojo;
+            if (bottom) {
+                scroller.revealBottom();
+            } else {
+                var chalk_el = this.controller
+                    .get('blockchalk-'+ this.chalk.id);
+                if (chalk_el) {
+                    var dim = chalk_el.getDimensions();
+                    scroller.revealElement(chalk_el);
+                    scroller.adjustBy(0, 0 - dim.height - 5);
+                }
+            }
         
             chain.next();
         },
@@ -129,33 +176,25 @@ ChalkAssistant.prototype = (function () { /** @lends ChalkAssistant# */
          * Hook up listeners on card activation.
          */
         activate: function (event) {
-
             var listeners = [
                 ['chalk-reply-button', Mojo.Event.tap, this.handleReply],
                 ['chalk-bury-button', Mojo.Event.tap, this.handleBury],
                 ['chalk-tweet-button', Mojo.Event.tap, this.handleTweet],
-                ['chalk-email-button', Mojo.Event.tap, this.handleEmail]
+                ['chalk-email-button', Mojo.Event.tap, this.handleEmail],
+                ['threadlist', Mojo.Event.listTap, this.handleChalkTap],
+                ['threadlist', Mojo.Event.listDelete, this.handleBuryChalk]
             ];
             if (['nearby','home'].indexOf(BlockChalk.service.getLocationContext()) !== -1) {
                 // Wire up the chalkback button if allowed.
-                listeners.push([
-                    'chalk-chalkback-button', 
-                    Mojo.Event.tap, this.handleChalkback
-                ]);
+                listeners.push(['chalk-post', 
+                    Mojo.Event.tap, this.handlePostSubmit]);
+                listeners.push(['chalk-message',
+                    Mojo.Event.propertyChange, this.handlePostChange]);
             }
             if (this.chalk.place) {
                 // Wire up the location button if necessary.
-                listeners.push([
-                    'chalk-view-location-button', 
-                    Mojo.Event.tap, this.handleViewLocation
-                ]);
-            }
-            if (this.chalk.chalkbackTo) {
-                // Wire up the view chalkback button if necessary.
-                listeners.push([
-                    'chalk-view-chalkback-button', 
-                    Mojo.Event.tap, this.handleViewChalkback
-                ]);
+                listeners.push(['chalk-view-location-button', 
+                    Mojo.Event.tap, this.handleViewLocation]);
             }
 
             Decafbad.Utils.setupListeners(listeners, this);
@@ -233,23 +272,6 @@ ChalkAssistant.prototype = (function () { /** @lends ChalkAssistant# */
         },
 
         /**
-         * Handle tap on the view original chalkback button
-         */
-        handleViewChalkback: function (ev) {
-            BlockChalk.service.getChalk(
-                this.chalk.chalkbackTo,
-                function (chalk) {
-                    this.controller.stageController.pushScene('chalk', chalk);
-                }.bind(this),
-                function (resp) {
-                    Decafbad.Utils.showSimpleBanner(
-                        $L('Original chalk not found!')
-                    );
-                }.bind(this)
-            );
-        },
-
-        /**
          * Handle tap on the tweet button
          */
         handleTweet: function (ev) {
@@ -286,6 +308,118 @@ ChalkAssistant.prototype = (function () { /** @lends ChalkAssistant# */
                     }
                 }
             });
+        },
+
+        /**
+         * Update the remaining character counter, returning the count.
+         */
+        updateRemainingChars: function () {
+            var remaining = MAX_MESSAGE_LENGTH - this.model.message.length;
+            var counter = this.controller.get('character-count');
+            
+            if (remaining < 0) {
+                counter.addClassName('over');
+            } else {
+                counter.removeClassName('over');
+            }
+
+            counter.update(''+remaining);
+            return remaining;
+        },
+
+        /**
+         * Handle changes to the message field, mainly to update counter.
+         */
+        handlePostChange: function (ev) {
+            var remaining = this.updateRemainingChars();
+
+            // If enter is pressed, pretend the post button was tapped.
+            if (ev && Mojo.Char.isEnterKey(ev.originalEvent.keyCode)) {
+                this.handlePostSubmit(ev);
+            }
+        },
+
+        /**
+         * Handle a tap on the submit button to post the chalk.
+         */
+        handlePostSubmit: function (ev) {
+            
+            // Prevent duplicate submissions
+            if (this.model.submit_disabled) { return; }
+            
+            // Refuse to submit messages over the limit
+            var remaining = this.updateRemainingChars();
+            if (remaining < 0) { return; }
+
+            // Flag this submission as in progress
+            this.model.submit_disabled = true;
+            this.controller.modelChanged(this.model);
+
+            Decafbad.Utils.showLoadingSpinner(this);
+
+            BlockChalk.service.createNewChalk(
+                this.model.message, BlockChalk.gps_fix, BlockChalk.user_id,
+                this.chalk.id,
+                function (new_chalk) {
+
+                    Decafbad.Utils.hideLoadingSpinner(this);
+                    this.refresh(true);
+
+                }.bind(this),
+                function (resp) {
+
+                    Decafbad.Utils.hideLoadingSpinner(this);
+                    Decafbad.Utils.showSimpleBanner(
+                        $L('Chalk creation failed. Try again?')
+                    );
+                    this.model.submit_disabled = false;
+                    this.controller.modelChanged(this.model);
+
+                    Mojo.Log.error("CHALK FAILED %j", resp);
+
+                }.bind(this)
+            );
+
+        },
+
+        /**
+         * Summon a pop up menu on chalk thread list tap.
+         */
+        handleChalkTap: function (ev) {
+            this.controller.popupSubmenu({
+                placeNear: ev.target,
+                items: [
+                    { command: 'reply', label: 'Reply Privately' },
+                    { command: 'bury',  label: 'Report' }
+                ],
+                onChoose: function (command) {
+                    switch (command) {
+                        case 'reply':
+                            return this.controller.stageController
+                                .pushScene('reply', ev.item);
+                        case 'bury':
+                            return this.handleBuryChalk(ev);
+                    }
+                }.bind(this)
+            });
+        },
+
+        /**
+         * Bury a chalk to hide it from view.
+         */
+        handleBuryChalk: function (ev) {
+            Decafbad.Utils.showLoadingSpinner(this);
+            BlockChalk.service.buryChalk(
+                ev.item.id, BlockChalk.user_id,
+                function (resp) {
+                    Decafbad.Utils.hideLoadingSpinner(this);
+                    this.refresh();
+                }.bind(this),
+                function (resp) {
+                    Decafbad.Utils.hideLoadingSpinner(this);
+                    Decafbad.Utils.showSimpleBanner('Chalk report FAILED!');
+                }.bind(this)
+            );
         },
 
         EOF: null
